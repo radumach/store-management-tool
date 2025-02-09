559b322fe9a9d4f7f0b108ac1b2d41186dc323d0
radumach <radu.macovei@gmail.com>
2025-02-09 11:07:40 +0200

    add user model

---
+++ src/main/java/com/store/model/User.java
@@ -0,0 +1,81 @@
+package com.store.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        if (role.startsWith("ROLE_")) {
+            this.role = role.substring(5); // Remove "ROLE_" prefix
+        } else {
+            this.role = role;
+        }
+    }
+    
+    @JsonIgnore  // This prevents authorities from being serialized
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    // // Convert role to GrantedAuthority
+    // public Collection<? extends GrantedAuthority> getAuthorities() {
+    //     return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+    // }
+}
